e905418f9238e37f21926e3e0eb298f8a204e5ac
John Piedrahita <jhon.piedrahita@outlook.com>
2025-05-27 10:09:25 -0500

    Se adicionan propiedades para la impresión del documento

--- html_pdf/html_pdf.py
+++ html_pdf/html_pdf.py
@@ -7,7 +7,7 @@ from pyppeteer import launch
 
 async def convertir_html_a_pdf(ruta_html, ruta_pdf, formato_hoja, orientacion):
 
-    navegador = await launch()
+    navegador = await launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox'])
     pagina = await navegador.newPage()
     await pagina.setViewport({'width': 1200, 'height': 800})
 
